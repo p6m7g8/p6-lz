41c8785480cb32fb7444c7398cd3a0864bedaf80
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-11 13:46:44 -0600

    feat: flush out flow (#4)

--- src/deploy.ts
+++ src/deploy.ts
@@ -4,7 +4,9 @@ import process from 'node:process'
 import * as cdk from 'aws-cdk-lib'
 import { AuditAccountStack } from './audit'
 import { AVMStack } from './avm'
+import { LogarchiveAccountStack } from './logarchive'
 import { OrganizationStack } from './organization'
+import { SharedAccountStack } from './shared'
 
 const env = {
   account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
@@ -12,11 +14,27 @@ const env = {
 }
 
 const app = new cdk.App()
+const auditAccountId = app.node.tryGetContext('auditAccountId')
+const logarchiveAccountId = app.node.tryGetContext('logarchiveAccountId')
+const sharedAccountId = app.node.tryGetContext('sharedAccountId')
+
 new OrganizationStack(app, 'p6-lz-organization', { env })
-const avmStack = new AVMStack(app, 'p6-lz-avm', { env })
+new AVMStack(app, 'p6-lz-avm', { env })
 new AuditAccountStack(app, 'p6-lz-audit', {
   env: {
-    account: avmStack.auditAccountId,
+    account: auditAccountId,
+    region: env.region,
+  },
+})
+new LogarchiveAccountStack(app, 'p6-lz-logarchive', {
+  env: {
+    account: logarchiveAccountId,
+    region: env.region,
+  },
+})
+new SharedAccountStack(app, 'p6-lz-shared', {
+  env: {
+    account: sharedAccountId,
     region: env.region,
   },
 })
