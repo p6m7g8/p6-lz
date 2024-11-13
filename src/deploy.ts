068039da5e15f1601129b187ef8ba0b529d13527
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-13 18:49:20 -0500

    chore: closer/sync

--- src/deploy.ts
+++ src/deploy.ts
@@ -4,8 +4,11 @@ import process from 'node:process'
 import * as cdk from 'aws-cdk-lib'
 import { AuditAccountStack } from './audit'
 import { AVMStack } from './avm'
+import { DevAccountStack } from './dev'
 import { LogarchiveAccountStack } from './logarchive'
 import { OrganizationStack } from './organization'
+import { ProdAccountStack } from './prod'
+import { SandboxAccountStack } from './sandbox'
 import { SharedAccountStack } from './shared'
 
 const env = {
@@ -14,30 +17,66 @@ const env = {
 }
 
 const app = new cdk.App()
-const auditAccountId = app.node.tryGetContext('auditAccountId')
 const logarchiveAccountId = app.node.tryGetContext('logarchiveAccountId')
+const auditAccountId = app.node.tryGetContext('auditAccountId')
 const sharedAccountId = app.node.tryGetContext('sharedAccountId')
+const sandboxAccountId = app.node.tryGetContext('sandboxAccountId')
+const devAccountId = app.node.tryGetContext('devAccountId')
+const prodAccountId = app.node.tryGetContext('prodAccountId')
 
 new OrganizationStack(app, 'p6-lz-organization', { env })
 new AVMStack(app, 'p6-lz-avm', { env })
 
-const logarchiveStack = new LogarchiveAccountStack(app, 'p6-lz-logarchive', {
+const logarchiveAccountStack = new LogarchiveAccountStack(app, 'p6-lz-logarchive', {
   env: {
     account: logarchiveAccountId,
     region: env.region,
   },
 })
-new AuditAccountStack(app, 'p6-lz-audit', {
+const auditAccountStack = new AuditAccountStack(app, 'p6-lz-audit', {
   env: {
     account: auditAccountId,
     region: env.region,
   },
-}).addDependency(logarchiveStack)
-new SharedAccountStack(app, 'p6-lz-shared', {
+})
+auditAccountStack.addDependency(logarchiveAccountStack)
+
+const sharedAccountStack = new SharedAccountStack(app, 'p6-lz-shared', {
   env: {
     account: sharedAccountId,
     region: env.region,
   },
-}).addDependency(logarchiveStack)
+})
+sharedAccountStack.addDependency(logarchiveAccountStack)
+
+const sandboxAccountStack = new SandboxAccountStack(app, 'p6-lz-sandbox', {
+  env: {
+    account: sandboxAccountId,
+    region: env.region,
+  },
+})
+sandboxAccountStack.addDependency(sharedAccountStack)
+sandboxAccountStack.addDependency(auditAccountStack)
+sandboxAccountStack.addDependency(logarchiveAccountStack)
+
+const devAccountStack = new DevAccountStack(app, 'p6-lz-dev', {
+  env: {
+    account: devAccountId,
+    region: env.region,
+  },
+})
+devAccountStack.addDependency(sharedAccountStack)
+devAccountStack.addDependency(auditAccountStack)
+devAccountStack.addDependency(logarchiveAccountStack)
+
+const prodAccountStack = new ProdAccountStack(app, 'p6-lz-prod', {
+  env: {
+    account: prodAccountId,
+    region: env.region,
+  },
+})
+prodAccountStack.addDependency(sharedAccountStack)
+prodAccountStack.addDependency(auditAccountStack)
+prodAccountStack.addDependency(logarchiveAccountStack)
 
 app.synth()
