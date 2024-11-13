52d485bdf266c4bdf479dc43c6c87cd5ff5e5bda
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-12 20:10:26 -0500

    feat: maintain ci process with lint/build

--- src/organization.ts
+++ src/organization.ts
@@ -1,8 +1,5 @@
 import type { Construct } from 'constructs'
 import * as cdk from 'aws-cdk-lib'
-import * as iam from 'aws-cdk-lib/aws-iam'
-import * as ssm from 'aws-cdk-lib/aws-ssm'
-import * as floyd from 'cdk-iam-floyd'
 // import { CfnOrganization } from 'aws-cdk-lib/aws-organizations'
 
 export class OrganizationStack extends cdk.Stack {
@@ -12,10 +9,5 @@ export class OrganizationStack extends cdk.Stack {
     // const organization = new CfnOrganization(this, 'Organization', {
     //   featureSet: 'ALL',
     // })
-
-    // const organizationId: string = 'o-b1ngfg6w8x'
-    // const rootOuId: string = 'r-erd4'
-    // const accountId = cdk.Stack.of(this).account
-    // const organizationArn: string = `arn:aws:organizations::${accountId}:organization/${organizationId}`
   }
 }
