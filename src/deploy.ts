a063e90f09a9ee9a1114906ade7836b5621875e6
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-17 13:03:18 -0500

    fix

--- src/deploy.ts
+++ src/deploy.ts
@@ -2,12 +2,13 @@
 
 import process from 'node:process'
 import * as cdk from 'aws-cdk-lib'
+import { OrganizationStack } from './stacks/mgmt-organization'
 
-const _env = {
+const env = {
   account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
   region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION,
 }
 
 const app = new cdk.App()
-
+new OrganizationStack(app, 'p6-lz-mgmt-organization', { env })
 app.synth()
