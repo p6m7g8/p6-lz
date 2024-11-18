f4fc51bf5ef24bd46a77ca2d536318a2ac203d5c
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-17 22:02:53 -0600

    refactor: phase 2 (#27)

---
+++ test/management-avm-stack.test.ts
@@ -0,0 +1,12 @@
+import * as cdk from 'aws-cdk-lib'
+import { Template } from 'aws-cdk-lib/assertions'
+import { AvmStack } from '../src/stacks/management-avm'
+
+it('avm Stack includes P6LzSraAvmOus and P6LzSraAvmAccounts constructs', () => {
+  const app = new cdk.App()
+  const stack = new AvmStack(app, 'TestStack', { env: { account: '123456789012', region: 'us-east-1' } })
+
+  const template = Template.fromStack(stack)
+
+  template.resourceCountIs('AWS::CloudFormation::CustomResource', 2) // Validating both constructs are created
+})
