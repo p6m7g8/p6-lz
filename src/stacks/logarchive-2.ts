99a748e914122c3f7060f91b8ca26a64073359e0
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-19 15:21:15 -0500

    fix

--- src/stacks/logarchive-2.ts
+++ src/stacks/logarchive-2.ts
@@ -1,5 +1,4 @@
 import type { Construct } from 'constructs'
-import type { AccountAlias, ShareWithOrg } from '../types'
 import * as cdk from 'aws-cdk-lib'
 
 interface LogarchiveAccountStack2Props extends cdk.StackProps {}
