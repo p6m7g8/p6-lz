a5f714260b37933b2d2da99b95b467fc4dea0d77
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-19 18:05:56 -0500

    feat: setups configAggregrator in audit (#37)

--- src/stacks/audit-2.ts
+++ src/stacks/audit-2.ts
@@ -1,10 +1,49 @@
 import type { Construct } from 'constructs'
+import type { ShareWithOrg } from '../types'
 import * as cdk from 'aws-cdk-lib'
+import * as config from 'aws-cdk-lib/aws-config'
+import * as iam from 'aws-cdk-lib/aws-iam'
 
-interface AuditAccountStack2Props extends cdk.StackProps {}
+interface AuditAccountStack2Props extends cdk.StackProps, ShareWithOrg {}
 
 export class AuditAccountStack2 extends cdk.Stack {
   constructor(scope: Construct, id: string, props: AuditAccountStack2Props) {
     super(scope, id, props)
+
+    const configPrinciple = new iam.ServicePrincipal('config.amazonaws.com')
+    const role = new iam.Role(this, 'ConfigAggregatorRole', {
+      assumedBy: configPrinciple,
+    })
+    role.attachInlinePolicy(
+      new iam.Policy(this, 'ConfigAggregatorPolicy', {
+        statements: [
+          new iam.PolicyStatement({
+            actions: [
+              'config:DescribeConfigurationAggregators',
+              'config:DescribeConfigurationAggregatorSourcesStatus',
+              'config:DescribePendingAggregationRequests',
+              'config:GetAggregateComplianceDetailsByConfigRule',
+              'config:GetAggregateDiscoveredResourceCounts',
+              'config:BatchGetAggregateResourceConfig',
+              'config:SelectAggregateResourceConfig',
+              'config:ListAggregateDiscoveredResources',
+            ],
+            resources: ['*'],
+          }),
+          new iam.PolicyStatement({
+            actions: ['organizations:DescribeOrganization'],
+            resources: ['*'],
+          }),
+        ],
+      }),
+    )
+
+    new config.CfnConfigurationAggregator(this, 'P6LzSraConfigAggregator', {
+      configurationAggregatorName: 'P6LzSraConfigAggregator',
+      organizationAggregationSource: {
+        roleArn: role.roleArn,
+        allAwsRegions: true,
+      },
+    })
   }
 }
