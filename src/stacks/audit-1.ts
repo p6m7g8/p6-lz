add8bbceaaf42f077ab44f976714fe668fa6799f
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-19 13:58:13 -0500

    feat: Slackbot chatbot

--- src/stacks/audit-1.ts
+++ src/stacks/audit-1.ts
@@ -1,7 +1,9 @@
 import type { Construct } from 'constructs'
 import type { AccountAlias, LogarchiveBucketArn, ShareWithOrg } from '../types'
 import * as cdk from 'aws-cdk-lib'
+import * as chatbot from 'aws-cdk-lib/aws-chatbot'
 import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as sns from 'aws-cdk-lib/aws-sns'
 import { P6CDKNamer } from 'p6-cdk-namer'
 import { P6LzSraCloudWatch } from '../constructs/p6-lz-sra-cloudwatch'
 import { P6LzSraConfig } from '../constructs/p6-lz-sra-config'
@@ -22,6 +24,7 @@ export class AuditAccountStack1 extends cdk.Stack {
     })
 
     const bucket = s3.Bucket.fromBucketArn(this, 'CentralBucket', props.centralBucketArn.toString())
+
     const trail = new P6LzSraOrgTrail(this, 'P6LzSraOrgTrail', {
       logGroup: cw.logGroup,
       logRole: cw.logRole,
@@ -33,5 +36,15 @@ export class AuditAccountStack1 extends cdk.Stack {
       principals: props.principals,
       centralBucket: bucket,
     })
+
+    const snsTopic = new sns.Topic(this, 'P6LzTopicSlack', {
+      displayName: 'p6-lz-slack-notifications',
+    })
+    const slackChannel = new chatbot.SlackChannelConfiguration(this, 'MySlackChannel', {
+      slackChannelConfigurationName: 'p6-lz-notifications',
+      slackWorkspaceId: 'TMCK8D7S5',
+      slackChannelId: 'C081AG7GKEJ',
+    })
+    slackChannel.addNotificationTopic(snsTopic)
   }
 }
