948e443b1011d553d9c5284496563311f92f1c99
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-26 21:56:40 -0500

    feat: adds cdk-diag, cdk-nag, cfn-lint, cfn_nag

--- src/deploy.ts
+++ src/deploy.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import * as cdk from 'aws-cdk-lib'
+import { Aspects } from 'aws-cdk-lib'
+import { AwsSolutionsChecks, HIPAASecurityChecks, NIST80053R5Checks, PCIDSS321Checks } from 'cdk-nag'
 import { phase1, phase2, phase3, phase4 } from './phases'
 import { loadConfig } from './util'
 
@@ -8,6 +10,11 @@ async function main() {
   const config = await loadConfig()
 
   const app = new cdk.App()
+  Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }))
+  Aspects.of(app).add(new NIST80053R5Checks({ verbose: true }))
+  Aspects.of(app).add(new PCIDSS321Checks({ verbose: true }))
+  Aspects.of(app).add(new HIPAASecurityChecks({ verbose: true }))
+
   phase1(app, config)
   phase2(app, config)
   phase3(app, config)
